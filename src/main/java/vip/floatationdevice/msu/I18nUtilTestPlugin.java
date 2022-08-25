1ad744407562825da9009d020e354181609e58cd
MCUmbrella <mcumbrella@protonmail.com>
2022-08-25 15:53:37 +0800

    comments

--- src/main/java/vip/floatationdevice/msu/I18nUtilTestPlugin.java
+++ src/main/java/vip/floatationdevice/msu/I18nUtilTestPlugin.java
@@ -10,15 +10,23 @@ import java.io.File;
 
 import static vip.floatationdevice.msu.I18nUtil.translate;
 
+/**
+ * The example plugin that uses I18nUtil.
+ * @author MCUmbrella
+ */
 public class I18nUtilTestPlugin extends JavaPlugin implements CommandExecutor
 {
     YamlConfiguration cfg;
 
+    /**
+     * Load config.yml and initialize translation engine.
+     */
     void loadConfig()
     {
         File cfgFile = new File(getDataFolder(), "config.yml");
-        if(!cfgFile.exists()) saveDefaultConfig();
-        cfg = YamlConfiguration.loadConfiguration(cfgFile);
+        if(!cfgFile.exists()) saveDefaultConfig(); // create default config.yml if not exist
+        cfg = YamlConfiguration.loadConfiguration(cfgFile); // load it
+        // set the language used by I18nUtil. if there's no 'language' key in config.yml, use "en_US"
         I18nUtil.setLanguage(I18nUtilTestPlugin.class, cfg.getString("language", "en_US"));
     }
 
@@ -26,28 +34,28 @@ public class I18nUtilTestPlugin extends JavaPlugin implements CommandExecutor
     public void onEnable()
     {
         loadConfig();
-        getCommand("i18ntest").setExecutor(this);
+        getCommand("i18ntest").setExecutor(this); // register /i18ntest command
         getLogger().info("Language: " + I18nUtil.getLanguage() + " (" + cfg.getString("language", "en_US") + ") by " + I18nUtil.getLanguageFileContributor());
-        getLogger().info(translate("plugin-loaded"));
+        getLogger().info(translate("plugin-loaded")); // translate the 'plugin-loaded' key. the result depends on the value in lang_xx_XX.yml
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) // when someone used /i18ntest
     {
-        if(args.length == 0)
+        if(args.length == 0) // /i18ntest
         {
-            sender.sendMessage(translate("test-message"));
+            sender.sendMessage(translate("test-message")); // send the test message to the sender
             return true;
         }
-        else if(args.length == 1 && args[0].equals("reload"))
+        else if(args.length == 1 && args[0].equals("reload")) // /i18ntest reload
         {
-            loadConfig();
+            loadConfig(); // reload the config and I18nUtil
             sender.sendMessage("Plugin reloaded\nLanguage: " + I18nUtil.getLanguage() + " (" + cfg.getString("language", "en_US") + ") by " + I18nUtil.getLanguageFileContributor());
             return true;
         }
-        else if(args.length == 2 && args[0].equals("translate"))
+        else if(args.length == 2 && args[0].equals("translate")) // /i18ntest translate xxxxx
         {
-            sender.sendMessage(translate(args[1]));
+            sender.sendMessage(translate(args[1])); // translate the given key and send the result to the sender
             return true;
         }
         else return false;
